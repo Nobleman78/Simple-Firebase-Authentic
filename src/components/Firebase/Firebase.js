2b1fab041e1e8d5c7e090031d07569ed7b973eba
Jesmin-Chakma <jesminchakmacse@gmail.com>
2025-02-10 12:44:06 +0600

    React-Firebase

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-hmrLq2IRWGn4FtjlenppElW4CeFMj1Y",
+  authDomain: "simple-firebase-b65f3.firebaseapp.com",
+  projectId: "simple-firebase-b65f3",
+  storageBucket: "simple-firebase-b65f3.firebasestorage.app",
+  messagingSenderId: "54235336441",
+  appId: "1:54235336441:web:bf0501afb3c7963031dc28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
